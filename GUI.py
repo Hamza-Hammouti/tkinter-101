4bf0ca7aa5009b96312aeeef7ecfc4740ef19b49
HamzaHammouti <hamzahhammouti@gmail.com>
2022-04-15 02:12:48 +0200

    Explosief venster werkend en getest

---
+++ GUI.py
@@ -0,0 +1,31 @@
+import tkinter
+
+window = tkinter.Tk()
+
+window.title("My First Window")
+
+window.geometry("50x50")
+
+def colorChange(i=5):
+    if i >= 0:
+        colors = ('red', 'blue', 'green', 'black', 'yellow', 'white')
+        window.config(bg=colors[i])
+        window.after(2000, colorChange, i-1)
+        print(i+1)
+    if i == 4:
+        window.geometry("100x100")
+    if i == 3:
+        window.geometry("150x150")
+    if i == 2:
+        window.geometry("200x200")
+    if i == 1:
+        window.geometry("250x250")
+    if i == 0:
+        window.geometry("300x300")
+        print("BOOM!")
+        window.destroy()
+# -------------------------------
+
+colorChange()
+
+window.mainloop()
