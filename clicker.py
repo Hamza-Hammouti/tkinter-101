80a1cea76dd864ed99978fce847641c1a47458c4
HamzaHammouti <hamzahhammouti@gmail.com>
2022-05-17 11:53:48 +0200

    Clicker verkort

--- clicker.py
+++ clicker.py
@@ -8,27 +8,21 @@ window.configure(bg="grey")
 
 number = 0
 
-def up():
-    global number
-    number += 1
+def upanddown(aantal):
+    global number, button2
+    number += aantal
     if number >= 1:
         window.configure(bg="green")
-    elif number == 0:
-        window.configure(bg="grey")
-    button2 = tkinter.Label(window, text = number, width="30").grid(row=1, column=0, padx=15, pady=15)
-
-def down():
-    global number
-    number -= 1
-    if number <= -1:
+    elif number <= -1:
         window.configure(bg="red")
     elif number == 0:
         window.configure(bg="grey")
+
     button2 = tkinter.Label(window, text = number, width="30").grid(row=1, column=0, padx=15, pady=15)
 
-button1 = tkinter.Button(window, text ="Up", width="30", command=up).grid(row=0, column=0, padx=15, pady=15)
+button1 = tkinter.Button(window, text ="Up", width="30", command=lambda: [upanddown(1)]).grid(row=0, column=0, padx=15, pady=15)
 button2 = tkinter.Label(window, text = number, width="30").grid(row=1, column=0, padx=15, pady=15)
-button3 = tkinter.Button(window, text ="Down", width="30", command=down).grid(row=2, column=0, padx=15, pady=15)
+button3 = tkinter.Button(window, text ="Down", width="30", command=lambda: [upanddown(-1)]).grid(row=2, column=0, padx=15, pady=15)
 
 # -------------------------------
 
